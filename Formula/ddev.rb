eb4bbe9b30e27623306c5f4e40bb25bf25ac7358
Randy Fay <randy@randyfay.com>
2019-09-18 14:29:19 -0600

    Bump to v1.11.0-rc2

--- Formula/ddev.rb
+++ Formula/ddev.rb
@@ -1,8 +1,8 @@
 class Ddev < Formula
   desc "ddev: a local development environment management system"
   homepage "https://ddev.readthedocs.io/en/stable/"
-  url "https://github.com/drud/ddev/archive/v1.11.0-rc1.tar.gz"
-  sha256 "231980712bc37f9d57b91cf6a783072bd3ffcbab214e6fbcf360ee415f514ab1"
+  url "https://github.com/drud/ddev/archive/v1.11.0-rc2.tar.gz"
+  sha256 "5eab4c5f69a81dcf31f962dc62b704148ec220f5f801171d1afe8bd500d8c2a1"
 
   # depends_on "docker" => :run
   # depends_on "docker-compose" => :run
@@ -12,10 +12,10 @@ class Ddev < Formula
   depends_on "nss" => :run
 
   bottle do
-    root_url "https://github.com/drud/ddev/releases/download/v1.11.0-rc1/"
+    root_url "https://github.com/drud/ddev/releases/download/v1.11.0-rc2/"
     cellar :any_skip_relocation
-    sha256 "70f79ad87cb0d1698890275ffa69652a6d5435d9e61eb569ca63473000531da2" => :x86_64_linux
-    sha256 "294f9cbab33e793f49ddca9c8e4c7e903e0ff865028a54ba5d1ac0125302b3ec" => :sierra
+    sha256 "b28a0e6149d8081ab81e4d9562199d49595607648259ab83cbed3dabfe862178" => :x86_64_linux
+    sha256 "a2a1ddaa42c4cce81e59e42c5d0a1154032139c903cd3cff6498da8756c7a5f2" => :sierra
   end
   def install
     system "make", "VERSION=v#{version}", "COMMIT=v#{version}"
