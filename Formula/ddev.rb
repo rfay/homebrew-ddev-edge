0175f8b2cb0546903303402090e612995e45726b
rfay <randy@randyfay.com>
2020-11-09 19:27:54 +0000

    Homebrew bump to v1.16.0-rc3

--- Formula/ddev.rb
+++ Formula/ddev.rb
@@ -1,8 +1,8 @@
 class Ddev < Formula
   desc "ddev: a local development environment management system"
   homepage "https://ddev.readthedocs.io/en/stable/"
-  url "https://github.com/drud/ddev/archive/v1.16.0-rc2.tar.gz"
-  sha256 "f55ea72c2ae99629ddabea011bf5f446e575c180e19409db5b400d6253cebef5"
+  url "https://github.com/drud/ddev/archive/v1.16.0-rc3.tar.gz"
+  sha256 "b5d70cbca1b9738cc0d8d44080c7da7163b046538c30b44b2f903680ff64a50e"
 
   # depends_on "docker" => :run
   # depends_on "docker-compose" => :run
@@ -12,10 +12,10 @@ class Ddev < Formula
   depends_on "nss" => :run
 
   bottle do
-    root_url "https://github.com/drud/ddev/releases/download/v1.16.0-rc2/"
+    root_url "https://github.com/drud/ddev/releases/download/v1.16.0-rc3/"
     cellar :any_skip_relocation
-    sha256 "87b3b60b00b32c91d9e7eb63d232787d471da7f54035348e515a8f3d8c7aef4d" => :x86_64_linux
-    sha256 "c97c8a1fb54730d82e38cfda1b41ea58a6ad109b97725017509ead87c7e54241" => :high_sierra
+    sha256 "39d772448de2a87c1ce711b92ee6482c565e80b09116ba6ee7c474c648d3b501" => :x86_64_linux
+    sha256 "83ff17323559f9f4973be9b6c44a15cebe4bd2ea3bc9e6b8ede1db0a94595e28" => :high_sierra
   end
   def install
     system "make", "VERSION=v#{version}", "COMMIT=v#{version}"
