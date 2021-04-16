9010cf2e9c280990c2748abbb5900403c789bffd
rfay <randy@randyfay.com>
2021-04-16 00:06:29 +0000

    Homebrew bump to v1.17.2

--- Formula/ddev.rb
+++ Formula/ddev.rb
@@ -1,18 +1,17 @@
 class Ddev < Formula
   desc "Local development environment management system"
   homepage "https://ddev.readthedocs.io/en/stable/"
-  url "https://github.com/rfay/ddev/archive/v1.17.1.tar.gz"
-  sha256 "c4e1d0689ef380eabca3e28d6aafd87f2eeabc2df3f6e7dfc6944db9ed3fc0c8"
+  url "https://github.com/rfay/ddev/archive/v1.17.2.tar.gz"
+  sha256 "bbdafeddc4ca8e0f63894a3217667d1993f61ee1b11887edf594e47a38c539bb"
 
   depends_on "mkcert" => :run
   depends_on "nss" => :run
 
   bottle do
-    root_url "https://github.com/rfay/ddev/releases/download/v1.17.1/"
-    cellar :any_skip_relocation
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "09b0de530c9a729b4588a568300cf8053759799cc3317e0f96f5c6f4004a6225"
-    sha256 cellar: :any_skip_relocation, high_sierra: "97e78e11ca89d6f48400ee3e669dbba86d5f84c7ec22da629dbc1a08736e07cb"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f93699f3f381898f0ffb09d87ff1e93a870ba279e06921df64d5204562cbe9f6"
+    root_url "https://github.com/rfay/ddev/releases/download/v1.17.2/"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "187b5007b171ffa89fcc8563db7e35659b12b98e2d0fda8d46c71c83c0c3f62e"
+    sha256 cellar: :any_skip_relocation, high_sierra: "b2cf3301fce847fc1fc9ef2bddcce1d045a7ffcd5382246b6e813ba3493eb50f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c43158f531666994e752e627094223d12a50124303ddd2cbea682dbf1f56f5b7"
   end
   def install
     system "make", "VERSION=v#{version}", "COMMIT=v#{version}"
